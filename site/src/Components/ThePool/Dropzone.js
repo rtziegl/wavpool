1eda3000ddc44b8df33d406f5cb1e7c843dbad46
Ryley Ziegler <ryleyziegler@Ryleys-MacBook-Pro.local>
2022-12-29 23:31:42 -0500

    Beginning design for the pool.

--- site/src/Components/ThePool/Dropzone.js
+++ site/src/Components/ThePool/Dropzone.js
@@ -54,15 +54,17 @@ export default function DragDropFile() {
     };
     
     return (
-      <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
-        <input ref={inputRef} type="file" id="input-file-upload" multiple={true} onChange={handleChange} />
-        <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? "drag-active" : "" }>
-          <div>
-            <p>Drag and drop your file here or</p>
-            <button className="upload-button" onClick={onButtonClick}>Upload a file</button>
-          </div> 
-        </label>
-        { dragActive && <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div> }
-      </form>
+    <div className="wrap">
+        <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
+            <input ref={inputRef} type="file" id="input-file-upload" multiple={true} onChange={handleChange} />
+            <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? "drag-active" : "" }>
+           
+            </label>
+            { dragActive && <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div> }
+        </form>
+        <div className="spacer">
+                <button className="upload-button" onClick={onButtonClick}>Upload a file</button>
+        </div>
+      </div>
     );
   };
--- site/src/Components/ThePool/ThePool.js
+++ site/src/Components/ThePool/ThePool.js
@@ -1,11 +1,12 @@
 import Dropzone from "./Dropzone";
 import React, { useCallback, useState } from "react";
+import "./thepool.css";
 
 export default function ThePool(){
     return(
         <div>
+            <h2 className="pool-header">Drop your beat in the pool to start your upload.</h2>
             <Dropzone />
-            ThePool
         </div>
     );
 }
