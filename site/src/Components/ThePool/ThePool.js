99658dd031e716dbe2186441517a9a3951128917
Ryley Ziegler <ryleyziegler@Ryleys-MacBook-Pro.local>
2022-12-29 22:33:39 -0500

    Drag and drop file, then plays audio for now.

--- site/src/Components/ThePool/ThePool.js
+++ site/src/Components/ThePool/ThePool.js
@@ -1,6 +1,10 @@
+import Dropzone from "./Dropzone";
+import React, { useCallback, useState } from "react";
+
 export default function ThePool(){
     return(
         <div>
+            <Dropzone />
             ThePool
         </div>
     );
