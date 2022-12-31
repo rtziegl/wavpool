2e85ace5018a11e1f0ebeb3cb5e0540049bdd21f
Ryley Ziegler <ryleyziegler@Ryleys-MacBook-Pro.local>
2022-12-31 15:12:59 -0500

    Fix the drop off look.

--- site/src/Components/ThePool/Dropzone.js
+++ site/src/Components/ThePool/Dropzone.js
@@ -56,15 +56,17 @@ export default function DragDropFile() {
     return (
     <div className="wrap">
         <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
-            <input ref={inputRef} type="file" id="input-file-upload" multiple={true} onChange={handleChange} />
+            <input ref={inputRef} type="file" id="input-file-upload" multiple={false} onChange={handleChange} />
             <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? "drag-active" : "" }>
-           
+                <div className="around-form">
+                    <p>Drag and drop your file here or</p>
+                    <button className="upload-button" onClick={onButtonClick}>Upload a file.</button>
+                </div> 
             </label>
-            { dragActive && <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div> }
+            { dragActive && 
+            <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}> Drop it.</div>
+             }
         </form>
-        <div className="spacer">
-                <button className="upload-button" onClick={onButtonClick}>Upload a file</button>
-        </div>
       </div>
     );
   };
--- site/src/Components/ThePool/ThePool.js
+++ site/src/Components/ThePool/ThePool.js
@@ -5,7 +5,6 @@ import "./thepool.css";
 export default function ThePool(){
     return(
         <div>
-            <h2 className="pool-header">Drop your beat in the pool to start your upload.</h2>
             <Dropzone />
         </div>
     );
