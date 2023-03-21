8a2b05e8ef498d6cd5177521b77b2cd2520b8a5b
Ryley Ziegler <ryleyziegler@Ryleys-MacBook-Pro.local>
2023-03-20 20:42:11 -0400

    Leaderboard beginning functionality working.

--- site/src/Components/Leaderboard/Leaderboard.js
+++ site/src/Components/Leaderboard/Leaderboard.js
@@ -2,12 +2,13 @@ import './leaderboard.css';
 import React, { useEffect, useState } from "react";
 import abi from "../../contract_utils/Competition.json";
 const { ethers } = require("ethers");
-export default function Leaderboard(){
-    let allCompets = [];
-
+const web3 = require('web3');
+export default function Leaderboard() {
+    const [competitors, setCompetitors] = useState([]);
     const getLeaderboardStats = async () => {
         const contractAddress = "0x62c921f6EFf43333970Ae7650BF9aa110D2b28D2";
         const contractABI = abi.abi;
+        let amtOfLeaders = []
 
         try {
             const { ethereum } = window;
@@ -17,16 +18,29 @@ export default function Leaderboard(){
                 const signer = provider.getSigner();
                 const compContract = new ethers.Contract(contractAddress, contractABI, signer);
                 let allCompetitors = await compContract.getAllUsers()
-                console.log(allCompetitors)
 
                 for (let i = 0; i < allCompetitors.length; i++) {
-                    let competitorInfo = await compContract.getCompetitorStats(allCompetitors[i])
-                    console.log(competitorInfo)
-                    allCompets.push(competitorInfo)
+                    let competitorInfo = await compContract.getCompetitorStats(allCompetitors[i]);
+                    for (let j = 0; j < competitorInfo[2].length; j++) {
+                        amtOfLeaders.push(web3.utils.hexToNumber(competitorInfo[2][j]));
+                    }
+                    const updateCompetitors = [
+                        ...competitors,
+                        {
+                            id: competitors.length + 1,
+                            address: competitorInfo[0],
+                            gainedVotesAllTime: web3.utils.hexToNumber(competitorInfo[1]),
+                            amtOfFirst: amtOfLeaders[0],
+                            amtOfSecond: amtOfLeaders[1],
+                            amtOfThird: amtOfLeaders[1],
+                            amtOfCompsEntered: web3.utils.hexToNumber(competitorInfo[3]),
+                            nftCountAllTime: web3.utils.hexToNumber(competitorInfo[4]),
+                        }
+                    ];
+                    setCompetitors(updateCompetitors)
                 }
-                console.log(allCompets)
             }
-            
+
         } catch (error) {
             console.log(error)
         }
@@ -36,9 +50,22 @@ export default function Leaderboard(){
         getLeaderboardStats()
     }, [])
 
-    return(
+    return (
         <div>
             Leaderboard
+            <ul>
+                {competitors.map(competitor => (
+                    <li key={competitor.id}>
+                        <p>{competitor.address}</p>
+                        <p>{competitor.amtOfFirst}</p>
+                        <p>{competitor.amtOfSecond}</p>
+                        <p>{competitor.amtOfThird}</p>
+                        <p>{competitor.gainedVotesAllTime}</p>
+                        <p>{competitor.amtOfCompsEntered}</p>
+                        <p>{competitor.nftCountAllTime}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
